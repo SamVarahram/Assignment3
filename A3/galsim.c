c079235cc293d99b18a6fc524a34f696b5b0e9c5
Sam Varahram <sava3149@vitsippa.it.uu.se>
2025-02-03 13:21:33 +0100

    makefile created

--- A3/galsim.c
+++ A3/galsim.c
@@ -1,5 +1,16 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char** argv[]) {
-    
+    if(argc != 6) {
+        fprintf(stderr, "Usage: %s <Number of stars> <input file> <Number of timesteps> <size of timesteps> <graphics>\n", argv[0]);  return 1;
+    }
+    int nstars = atoi(argv[1]);
+    char** input_file = argv[2];
+    int nsteps = atoi(argv[3]);
+    int stepsize = atoi(argv[4]);
+    bool graphics = atoi(argv[5]); // 0 or 1 for false or true
 }
+
