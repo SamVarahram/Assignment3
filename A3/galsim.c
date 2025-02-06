afa0af2534521d1176a050a01d569bf4e9d70b61
Sam Varahram <sava3149@vitsippa.it.uu.se>
2025-02-06 13:43:37 +0100

    Only one problem left, uninitialised variable at main

--- A3/galsim.c
+++ A3/galsim.c
@@ -11,10 +11,10 @@ typedef struct {
 } Vector2D;
 
 // Function prototypes
-Vector2D get_force_on_body(const int nstars, const int G, const float e0, int i, Vector2D* position, const double* mass);
+Vector2D get_force_on_body(const int nstars, const int G, const float e0, int i, Vector2D* position, double* mass);
 double get_rij(int i, int j, Vector2D* position);
 Vector2D get_position_vector(int i, int j, Vector2D* position);
-void update_velocity_and_position(int i, const int stepsize, Vector2D* velocity, Vector2D* position, Vector2D* F, const double* mass);
+void update_velocity_and_position(int i, const int stepsize, Vector2D* velocity, Vector2D* position, Vector2D* F, double* mass);
 
 
 
@@ -28,26 +28,39 @@ int main(int argc, char* argv[]) {
     const char* input_file = argv[2];
     const int nsteps = atoi(argv[3]);
     const int stepsize = atoi(argv[4]);
-    const bool graphics = atoi(argv[5]); // 0 or 1 for false or true
+    const int graphics = atoi(argv[5]); // 0 or 1 for false or true
+    if (graphics) {
+        fprintf(stderr, "Graphics not supported\n");
+        return 1;
+    }
 
     const int G = 100/nstars;
     const double e0 = 0.001; // Softening factor 10^-3
 
     // Create arrays to store the data and Vector2D to store the forces
     Vector2D* position = (Vector2D*) malloc(nstars * sizeof(Vector2D));
-    const double* mass = (double*) malloc(nstars * sizeof(double));
+    double* mass = (double*) malloc(nstars * sizeof(double));
     Vector2D* velocity = (Vector2D*) malloc(nstars * sizeof(Vector2D));
-    const bool* brightness = (double*) malloc(nstars * sizeof(double));
+    double* brightness = (double*) malloc(nstars * sizeof(double));
 
     if (position == NULL || mass == NULL || velocity == NULL || brightness == NULL) {
         fprintf(stderr, "Error allocating memory\n");
         return 1;
     }
+    // Initialize arrays to zero
+    memset(position, 0, nstars * sizeof(Vector2D));
+    memset(mass, 0, nstars * sizeof(double));
+    memset(velocity, 0, nstars * sizeof(Vector2D));
+    memset(brightness, 0, nstars * sizeof(double));
 
     // Read in the data
-    FILE* file = fopen(input_file, "r");
+    FILE* file = fopen(input_file, "rb");
     if(file == NULL) {
         fprintf(stderr, "Error opening file\n");
+        free(position);
+        free(mass);
+        free(velocity);
+        free(brightness);
         return 1;
     }
 
@@ -62,24 +75,31 @@ int main(int argc, char* argv[]) {
     particle 1 position x*/
     // The input file is binary
     for (int i = 0; i < nstars; i++) {
-        fread(&position[i], sizeof(Vector2D), 1, file);
-        fread(&mass[i], sizeof(double), 1, file);
-        fread(&velocity[i], sizeof(Vector2D), 1, file);
-        fread(&brightness[i], sizeof(bool), 1, file);
+        if (fread(&position[i], sizeof(Vector2D), 1, file) != 1 ||
+            fread(&mass[i], sizeof(double), 1, file) != 1 ||
+            fread(&velocity[i], sizeof(Vector2D), 1, file) != 1 ||
+            fread(&brightness[i], sizeof(double), 1, file) != 1) {
+            fprintf(stderr, "Error reading file\n");
+            return 1;
+        }
+        // Print values for debugging
+        // printf("Star %d: Position (%f, %f), Mass %f, Velocity (%f, %f), Brightness %f\n",
+        //    i, position[i].x, position[i].y, mass[i], velocity[i].x, velocity[i].y, brightness[i]);
     }
     fclose(file);
-
+    Vector2D F = {0.0, 0.0};
     // Loop over the timesteps
     for (int time = 0; time < nsteps; time++) {
         for (int i = 0; i < nstars; i++) {
-            Vector2D F = get_force_on_body(nstars, G, e0, i, position, mass);
+            F = get_force_on_body(nstars, G, e0, i, position, mass);
             update_velocity_and_position(i, stepsize, velocity, position, &F, mass);
         }
     }
+    
 
 
     // Output the data in a binary file
-    FILE* output = fopen("output.bin", "wb");
+    FILE* output = fopen("result.gal", "wb");
     if(output == NULL) {
         fprintf(stderr, "Error opening file\n");
         return 1;
@@ -88,51 +108,42 @@ int main(int argc, char* argv[]) {
         fwrite(&position[i], sizeof(Vector2D), 1, output);
         fwrite(&mass[i], sizeof(double), 1, output);
         fwrite(&velocity[i], sizeof(Vector2D), 1, output);
-        fwrite(&brightness[i], sizeof(bool), 1, output);
+        fwrite(&brightness[i], sizeof(double), 1, output);
     }
+    fclose(output);
 
     // Free the memory
     free(position);
     free(mass);
     free(velocity);
     free(brightness);
-    // Jag bytte till test
+    
 
 }
 // Function to calculate the force on a body
-Vector2D get_force_on_body(const int nstars, const int G, const float e0, int i, Vector2D* position, const double* mass) {
-    Vector2D F;
+Vector2D get_force_on_body(const int nstars, const int G, const float e0, int i, Vector2D* position, double* mass) {
+    Vector2D F = {0.0, 0.0};
     F.x = -G * mass[i];
     F.y = -G * mass[i]; 
     for (int j = 0; j < nstars; j++) {
         if (i != j) {
-            double rij = get_rij(i, j, position);
-            Vector2D vector = get_position_vector(i, j, position);
-            double temp = mass[j] / pow(rij + e0, 3);
-            F.x += temp * vector.x;
-            F.y += temp * vector.y;
+
+            double dx = position[i].x - position[j].x;
+            double dy = position[i].y - position[j].y;
+            double rij = sqrt(dx * dx + dy * dy);
+            
+            double temp = mass[j] / ((rij + e0) * (rij + e0) * (rij + e0)); // pow(rij + e0, 3)
+            F.x += temp * dx;
+            F.y += temp * dy;
         }
     }
     return F;
 }
 
-// Function to calculate the distance between two bodies
-double get_rij(int i, int j, Vector2D* position) {
-    return sqrt(pow(position[i].x - position[j].x, 2) + pow(position[i].y - position[j].y, 2));
-}
-
-// Function to calculate the position vector between two bodies
-Vector2D get_position_vector(int i, int j, Vector2D* position) {
-    Vector2D rij;
-    rij.x = position[i].x - position[j].x;
-    rij.y = position[i].y - position[j].y;
-    return rij;
-}
-
 // Function to calculate the acceleration of a body
-void update_velocity_and_position(int i, const int stepsize, Vector2D* velocity, Vector2D* position, Vector2D* F, const double* mass) {
-    velocity[i+1].x += stepsize * F[i].x / mass[i];
-    velocity[i+1].y += stepsize * F[i].y / mass[i];
-    position[i+1].x += stepsize * velocity[i+1].x;
-    position[i+1].y += stepsize * velocity[i+1].y;
+void update_velocity_and_position(int i, const int stepsize, Vector2D* velocity, Vector2D* position, Vector2D* F, double* mass) {
+    velocity[i].x += stepsize * F->x / mass[i];
+    velocity[i].y += stepsize * F->y / mass[i];
+    position[i].x += stepsize * velocity[i].x;
+    position[i].y += stepsize * velocity[i].y;
 }
